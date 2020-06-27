dd5b06462ab17c9a1132fb270b40d0ca26b98868
Pedro Acosta <yandalar@gmail.com>
2020-06-27 01:31:40 -0300

    bot reply

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const discord = require('discord.js');
 const bot = new discord.Client();
 bot.on('ready', event => { console.log('bot logueado.') });
 bot.login(process.env.TOKEN);
+bot.on('message', msg => { msg.reply(msg.content) });
 
 const express = require('express');
 const app = express();
 app.listen(process.env.PORT, () => { console.log('servidor web iniciado')});
+
