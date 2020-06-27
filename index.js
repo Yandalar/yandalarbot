7cd2e1de491aba83569474b6cf5ea9065851bd09
Pedro Acosta <yandalar@gmail.com>
2020-06-27 19:06:26 -0300

    Some code clieaning

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const discord = require('discord.js');
-const bot = new discord.Client();
-bot.on('ready', event => { console.log('bot logueado.') });
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('ready', event => { 
+    console.log('Bot logueado.') 
+});
+
 bot.login(process.env.TOKEN);
-bot.on('message', msg => { msg.reply(msg.content) });
+
+bot.on('message', msg => { 
+    console.log(message.content); 
+});
 
 const express = require('express');
 const app = express();
